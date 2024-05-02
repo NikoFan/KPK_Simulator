8f0f08f839b87db3033a763d6ee2875a01c5c49b
NikoFan <92752734+NikoFan@users.noreply.github.com>
2024-05-02 20:36:49 +0300

    Создана доска blend; Создан переход по сценам;

--- Проект/KPK_Simulator/Assets/PlayerCode/Contact.cs
+++ Проект/KPK_Simulator/Assets/PlayerCode/Contact.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using Debug = UnityEngine.Debug;
 
 public class Contact : MonoBehaviour
 {
     // Проверка что пользователь контактирует с дверью
-    void contact(Collider palyer)
+    void OnTriggerEnter(Collider palyer)
     {
         // Если Тег объекта - Door
-        if (palyer.gameObject.tag == "Door")
+        if (palyer.gameObject.tag == "Player")
         {
             // Действие
             Debug.Log("DoorOpen");
+            SceneManager.LoadScene("SecondScene");
         }
     }
 }
